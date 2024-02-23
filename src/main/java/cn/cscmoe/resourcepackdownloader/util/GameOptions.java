7c0c695f64a1df2c3eeb70c236392c47b2f12109
HUANGHU1 <907322015@qq.com>
2024-02-24 02:00:33 +0800

    init

---
+++ src/main/java/cn/cscmoe/resourcepackdownloader/util/GameOptions.java
@@ -0,0 +1,19 @@
+package cn.cscmoe.resourcepackdownloader.util;
+
+import java.nio.file.Path;
+
+public class GameOptions {
+    public static String MINECRAFT_OPTIONS_FILENAME = "options.txt";
+
+    public GameOptions(Path minecraftPath) {
+
+    }
+
+    public void enableResourcePack(String ResourcePackName) {
+
+    }
+
+    public void disableResourcePack(String ResourcePackName) {
+
+    }
+}
